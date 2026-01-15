ec7685ca72f15a0f2040828d9cfd605c39e0c32a
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2026-01-15 12:11:03 +0530

    feat: enhance buildShowHandler to include nation name in responses

--- src/commands/memberHandler.ts
+++ src/commands/memberHandler.ts
@@ -3,6 +3,7 @@ import { AllyError, STATIC_ERROR_CODES, throwStaticError } from '../shared/allyE
 import { getManagedChannelDataFromChannelId } from '../services/guildService.js';
 import { AllyManagedChannel } from '../@types/guilds.js';
 import { buildDiscordEmbed, EmbedType } from '../shared/discordEmbedBuilder.js';
+import { getSingleNationByNationId } from '../services/nationService.js';
 
 const buildShowHandler = async (command: ChatInputCommandInteraction) => {
     const { guildId, channelId } = command;
@@ -12,18 +13,30 @@ const buildShowHandler = async (command: ChatInputCommandInteraction) => {
         throwStaticError(STATIC_ERROR_CODES.TICKET_NOT_LINKED, 'buildShowHandler');
     }
 
-    const { templates } = channelData as AllyManagedChannel;
+    const { templates, nation_id } = channelData as AllyManagedChannel;
+    const nationData = await getSingleNationByNationId(parseInt(nation_id, 10));
+    const nationName = nationData?.nation_name;
     if (!templates?.build) {
-        await command.editReply({
+        return await command.editReply({
             embeds: [
                 buildDiscordEmbed({
                     type: EmbedType.member,
-                    title: `No build template is assigned to this channel`,
+                    title: `No build template is assigned to this channel${nationName && ` belonging to ${nationName}`}`,
                     description: `You can assign a build template to this channel by using \`/member build set\` command.`,
                 }),
             ],
         });
     }
+
+    await command.editReply({
+        embeds: [
+            buildDiscordEmbed({
+                type: EmbedType.member,
+                title: `🏢 Here is the current build${nationName && ` assigned to ${nationName}`}`,
+                description: templates.build,
+            }),
+        ],
+    });
 };
 
 export const memberHandler = async (command: ChatInputCommandInteraction) => {
